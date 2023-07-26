43fa7ef0757b846c53c0daa7002f30c039bece51
Harini Abeysirigunawardena <92200258+Harini487@users.noreply.github.com>
2023-07-26 13:31:19 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for(let i = 0; i < numberOfDrumButtons; i++){
+for(let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     //this - basically the identity of the button that triggered the event listener
     //see this by console.log(this) / console.log(this.innerHTML) - you get the assigned letter for each button class - w,a,s,d,j,k,l;
@@ -15,49 +15,11 @@ for(let i = 0; i < numberOfDrumButtons; i++){
 
     buttonAnimation(buttonInnerHTML); //For button animations
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio('./sounds/tom-1.mp3');
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio('./sounds/tom-2.mp3');
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio('./sounds/tom-3.mp3');
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio('./sounds/tom-4.mp3');
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio('./sounds/snare.mp3');
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio('./sounds/crash.mp3');
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio('./sounds/kick-bass.mp3');
-        kick.play();
-        break;
-
-      default: console.log(); //prints the buttoninnerHTML that triggered the default(if user inputs any other letter/ number, other than the above letters)
-    }
-    
   });
+
 }
 
-//Detecting Keyboard Press
+  //Detecting Keyboard Press
 
 document.addEventListener("keydown", function(event) { 
 
@@ -115,10 +77,10 @@ function makeSound(key) {
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);//as the same format as the classes of the buttons(".w", ".k", etc.)
   
-  activeButton.classList.add(".pressed"); //adding the pressed class in styles.css to the buttons to animate when selected
+  activeButton.classList.add("pressed"); //adding the pressed class in styles.css to the buttons to animate when selected
 
   setTimeout(function() {
-    activeButton.classList.remove(".pressed"); //This will remove the animation and bring back the original button style in 0.1sec
+    activeButton.classList.remove("pressed"); //This will remove the animation and bring back the original button style in 0.1sec
   }, 100);
 
 
